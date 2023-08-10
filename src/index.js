32aff30cecc9b631003f0daf9e90e0e4dfd25611
David <wjw3298@gmail.com>
2023-08-09 21:05:22 -0400

    complete with 2 version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
+// const movieImageDisplay = document.getElementById('movie-list')
+// const image = document.getElementById('detail-image')
+// const title = document.getElementById('title')
+// const releaseYear = document.getElementById('year-released')
+// const description = document.getElementById('description')
+// const bloodAmount = document.getElementById('amount')
+// const movieWatchedButton = document.getElementById('watched')
+// const bloodForm = document.getElementById('blood-form')
+
+
+// fetch('http://localhost:3000/movies')
+// .then(response => response.json())
+// .then(movieList => {
+//     movieList.forEach(movie => {
+//         showMovieImage(movie)
+//     })
+//     showMovieDetails(movieList[0])
+// })
+
+// function showMovieImage(movie){
+//     const image = document.createElement('img')
+//     image.src = movie.image
+//     movieImageDisplay.append(image)
+//     image.addEventListener('click', () => showMovieDetails(movie))
+// }
+
+// function showMovieDetails(movie){
+//     image.src = movie.image
+//     title.textContent = movie.title
+//     releaseYear.textContent = movie.release_year
+//     description.textContent = movie.description
+//     bloodAmount.textContent = movie.blood_amount
+
+//     if(movie.watched === false){
+//         return movieWatchedButton.textContent = "Unwatched"
+//     } else if( movie.watched === true){
+//         return movieWatchedButton.textContent = "Watched"
+//     }  
+// }
+
+// movieWatchedButton.addEventListener('click', () => {
+//     if(movieWatchedButton.textContent === "Unwatched"){
+//         return movieWatchedButton.textContent = "Watched"
+//     } else if(movieWatchedButton.textContent === "Watched" ){
+//         return movieWatchedButton.textContent = "Unwatched"
+//     }
+// })
+
+// bloodForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const amountOfBloodAdded = document.getElementById('blood-amount').value
+//     const totalAmountBlood = document.getElementById('amount')
+//     totalAmountBlood.textContent = parseInt(totalAmountBlood.textContent) + parseInt(amountOfBloodAdded)   
+// })
+
+
+
+//Version 1 blood amount presist when switch between movie
 fetch('http://localhost:3000/movies')
 .then(response => response.json())
 .then(movies => {
     createImg(movies)
     renderMovie(movies[0])
 })
-
 
 function createImg(movie){
     movie.forEach(movieDetail => {
@@ -19,7 +76,6 @@ function createImg(movie){
     })
 }
 
-
 function renderMovie(movie){
         const title = document.getElementById('title')
         const releaseYear = document.getElementById('year-released')
@@ -28,14 +84,12 @@ function renderMovie(movie){
         const watchedOrNot = document.getElementById('watched')
         const bloodAmount = document.getElementById('blood-amount')
 
-
         bloodAmount.textContent = movie.blood_amount
         image.src = movie.image
         title.textContent = movie.title
         releaseYear.textContent = movie.release_year
         description.textContent = movie.description
-
-
+        
         if (movie.watched === false){
             return watchedOrNot.textContent = 'Unwatch'
         } else if ( movie.watched === true) {
@@ -57,12 +111,10 @@ function buttonChange(button){
     }
 }
 
-//let bloodDrop = 0
 const input = document.getElementById('blood-form')
 input.addEventListener('submit', e => {
     e.preventDefault()
     const bloodAmount = document.getElementById('blood-amount').value
     const totalAmountOfBlood = document.getElementById('amount')
-    //bloodDrop = bloodDrop + parseInt(bloodAmount)
     totalAmountOfBlood.textContent = parseInt(totalAmountOfBlood.textContent) + parseInt(bloodAmount)
 })
